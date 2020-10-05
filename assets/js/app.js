417647afffd1132fc4d6bb4bab7006d589a0b608
maximiano leite <macio.pb.cg@gmail.com>
2020-10-05 15:33:53 -0300

    using multiple tags

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,36 @@ function Application () {
 
     return (
         buildHTML.createElement('div', { id: 'app'},
-            Header
+            Header,
+            buildHTML.createElement('table', {}, 
+                buildHTML.createElement('thead', {}, 
+                    buildHTML.createElement('tr', {}, 
+                        buildHTML.createElement('th', {}, '#'),
+                        buildHTML.createElement('th', {}, 'nome'),
+                        buildHTML.createElement('th', {}, 'email'),
+                        buildHTML.createElement('th', {}, 'ações')
+                    )
+                ),
+                buildHTML.createElement('tr', {},
+                    buildHTML.createElement('td', {}, '1'),
+                    buildHTML.createElement('td', {}, 'mariana melo'),
+                    buildHTML.createElement('td', {}, 'maria@gmail.com'),
+                    buildHTML.createElement('td', {}, 
+                        buildHTML.createElement('a', { href: '#'}, 'edit'),
+                        buildHTML.createElement('a', { href: '#'}, 'excluir')
+                    ),
+                ),
+                buildHTML.createElement('tr', {},
+                    buildHTML.createElement('td', {}, '1'),
+                    buildHTML.createElement('td', {}, 'mariana melo'),
+                    buildHTML.createElement('td', {}, 'maria@gmail.com'),
+                    buildHTML.createElement('td', {}, 
+                        buildHTML.createElement('a', { href: '#'}, 'edit'),
+                        buildHTML.createElement('a', { href: '#'}, 'excluir')
+                    ),
+                )
+            ),
+            buildHTML.createElement('input', {value: 'maximiano'})
         )
     );
 }
