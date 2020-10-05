8d9006d08b6887efbf0a61c6523066507eb4bc5c
maximiano leite <macio.pb.cg@gmail.com>
2020-10-05 14:33:40 -0300

    code sample width event click in link

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,17 +8,25 @@ const Header = () => {
             buildHTML.createElement('nav', {class: 'menu'}, 
                 buildHTML.createElement('ul', {class: 'menu__list'},
                     buildHTML.createElement('li', {class: 'menu__item'},
-                        buildHTML.createElement('a', { href: 'menu__link'},
+                        buildHTML.createElement('a', { class: 'menu__link',  href: '#'},
                             'Home'
                         )
                     ),
                     buildHTML.createElement('li', {class: 'menu__item'},
-                        buildHTML.createElement('a', { href: 'menu__link'},
+                        buildHTML.createElement('a', { class: 'menu__link',  href: '#'},
                             'About'
                         )
                     ),
-                    buildHTML.createElement('li', {class: 'menu__item'},
-                        buildHTML.createElement('a', { href: 'menu__link'},
+                    buildHTML.createElement('li', { class: 'menu__item', href: '#'},
+                        buildHTML.createElement('a', 
+                        { 
+                            class: 'menu__link',
+                            href: '#',
+                            click: function (event) {
+                                event.defalt
+                                console.log(event.target);
+                            }
+                        },
                             'Contact'
                         )
                     )
