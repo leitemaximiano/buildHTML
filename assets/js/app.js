88e3f397ee3e2646cbdbf4be48bdaded1864c912
maximiano leite <macio.pb.cg@gmail.com>
2020-10-05 15:18:34 -0300

    code sample

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,76 @@
-
-
-const Header = () => {
+function Application () {
 
     return (
-        buildHTML.createElement('header', { class: 'header' }, 
-            buildHTML.createElement('a', { href: '#'}, 'traça'),
-            buildHTML.createElement('nav', {class: 'menu'}, 
-                buildHTML.createElement('ul', {class: 'menu__list'},
-                    buildHTML.createElement('li', {class: 'menu__item'},
-                        buildHTML.createElement('a', { class: 'menu__link',  href: '#'},
-                            'Home'
-                        )
-                    ),
-                    buildHTML.createElement('li', {class: 'menu__item'},
-                        buildHTML.createElement('a', { class: 'menu__link',  href: '#'},
-                            'About'
-                        )
-                    ),
-                    buildHTML.createElement('li', { class: 'menu__item', href: '#'},
-                        buildHTML.createElement('a', 
-                        { 
-                            class: 'menu__link',
-                            href: '#',
-                            click: function (event) {
-                                event.defalt
-                                console.log(event.target);
-                            }
+        buildHTML.createElement('div', { id: 'app'},
+            Header
+        )
+    );
+}
+
+function Header () {
+    return (
+
+        buildHTML.createElement('header',
+        {
+            class: 'header'
+        },
+            buildHTML.createElement('a',
+            {
+                class: 'logo',
+                href: '#'
+            },
+            'Logo'
+            ),
+            buildHTML.createElement('nav',
+            {
+                class: 'menu'
+            },
+                buildHTML.createElement('ul',
+                {
+                    class: 'menu__list'
+                },
+                    buildHTML.createElement('li',
+                    {
+                        class: 'menu__item'
+                    },
+                        buildHTML.createElement('a',
+                        {
+                            class: 'menu__item',
+                            href: '#'
                         },
-                            'Contact'
+                          'HOME'  
+                        )
+                    ),
+                    buildHTML.createElement('li',
+                    {
+                        class: 'menu__item'
+                    },
+                        buildHTML.createElement('a',
+                        {
+                            class: 'menu__item',
+                            href: '#'
+                        },
+                          'HOME'  
+                        )
+                    ),
+                    buildHTML.createElement('li',
+                    {
+                        class: 'menu__item'
+                    },
+                        buildHTML.createElement('a',
+                        {
+                            class: 'menu__item',
+                            href: '#'
+                        },
+                          'HOME'  
                         )
                     )
                 )
+
             )
         )
-    )
+    );
 }
 
+buildHTML.renderHTML('#app', Application);
 
-buildHTML.renderHTML('#app', Header)
